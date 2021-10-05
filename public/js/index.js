5f5343cd21932ad63accc98021d0cfcb60eea076
evmir <flarx01@gmail.com>
2021-10-05 16:33:32 +0300

    Исправления в верстке
    * Добавил нулевой блок в css
    * В HTML отдельный id у score
    * Добавил обновление игрового поля

--- public/js/index.js
+++ public/js/index.js
@@ -5,14 +5,53 @@ function onKeyup(e){
     if (!gameEvent){
         return;
     }
-    gameEvent.call(logicInstance);
-    logicInstance.spawn();
+    gameEvent.call(gameLogic);
+    gameLogic.spawn();
+    const currentScore = gameLogic.score();
+    const currentField = gameLogic.gameField();
     printGameField();
-    console.log(logicInstance.score());
+    updateGrid(currentField);
+    updateScore(currentScore);
 }
+
+const ids = {
+    game: "game-field",
+    score: "score",
+    failBanner: "fail-banner",
+    winBanner: "win-banner"
+}
+
+const blockCss = "game__block game__block__";
+const game = document.getElementById(ids.game);
+const score = document.getElementById(ids.score);
+
+function updateGrid(field){
+    const childrens = Array.from(game.children);
+    Object.keys(childrens)
+        .forEach((childrenIndex) => {
+            const v = field[childrenIndex];
+            const block = childrens[childrenIndex];
+            const blockV = +block.innerHTML;
+            if (v === blockV){
+                return;
+            }
+            block.innerHTML = v;
+            block.className = blockCss + v;
+        })
+}
+function populateGrid(field){
+    game.innerHTML = '';
+    field.forEach((v) => {
+        let div = document.createElement('div');
+        div.innerHTML = v;
+        div.className = blockCss + v;
+        game.append(div);
+    })
+}
+
 function printGameField(){
     console.log('\n');
-    const gameField = [...logicInstance.gameField()];
+    const gameField = [...gameLogic.gameField()];
     let rowArr = [];
     for(let row = 0, column = 0; row < 4;){
         const v = gameField[row * 4 + column];
@@ -26,23 +65,41 @@ function printGameField(){
         }
     }
 }
+
+function updateScore(s){
+    score.innerText = s;
+}
 const testInstance = [
     0, 0, 0, 0,
     0, 0, 0, 0,
     0, 0, 0, 0,
-    4, 4, 2, 2
+    0, 0, 0, 0
 ];
+// const testInstance = [
+//     0, 4, 4, 0,
+//     2, 0, 0, 2,
+//     4, 0, 0, 2,
+//     4, 4, 2, 2
+// ];
+//todo: проверить 2 0 0 2
+
+//4, 4, 2, 2
 //expect 0 0 8 4 in last row
-const logicInstance = new GameLogic(testInstance);
-// logicInstance.spawn();
-// logicInstance.spawn();
-console.log(logicInstance);
-printGameField();
+const gameLogic = new GameLogic(testInstance);
+
 
 this.onkeyup = onKeyup;
 const keymap = new Map([
-    ['ArrowUp', logicInstance.up],
-    ['ArrowRight', logicInstance.right],
-    ['ArrowDown', logicInstance.down],
-    ['ArrowLeft', logicInstance.left],
+    ['ArrowUp', gameLogic.up],
+    ['ArrowRight', gameLogic.right],
+    ['ArrowDown', gameLogic.down],
+    ['ArrowLeft', gameLogic.left],
 ]);
+
+window.onload = (e) => {
+    gameLogic.spawn();
+    gameLogic.spawn();
+    printGameField();
+    const currentField = gameLogic.gameField()
+    populateGrid(currentField);
+}
